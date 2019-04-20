801276b0de6696a46b06809a1859a8e001ebec61
unknown <laurabatmanp@gmail.com>
2019-04-19 21:25:50 -0300

    cenarios

---
+++ dicionario_de_cenarios_ep1.py
@@ -0,0 +1,153 @@
+        "inicio": {
+            "titulo": "Saguao do perigo",
+            "descricao": "Voce esta no saguao de entrada do insper",
+            "opcoes": {
+                "andar professor": "Tomar o elevador para o andar do professor",
+                "biblioteca": "Ir para a biblioteca"
+                "auditorio":"ir para o auditorio"
+                
+            }
+        },
+        "andar professor": {
+            "titulo": "Andar do desespero",
+            "descricao": "Voce chegou ao andar da sala do seu professor",
+            "opcoes": {
+                "inicio": "Tomar o elevador para o saguao de entrada",
+                "professor": "Falar com o professor"
+            }
+        },
+        "professor": {
+            "titulo": "O monstro do Python",
+            "descricao": "Voce foi pedir para o professor adiar o EP. "
+                         "O professor revelou que é um monstro disfarçado "
+                         "e devorou sua alma.",
+            "opcoes": {}
+        },
+        "biblioteca": {
+            "titulo": "Caverna da tranquilidade",
+            "descricao": "Voce esta na biblioteca",
+            "opcoes": {
+                "inicio": "Voltar para o saguao de entrada",
+                "ler um livro": "estudar para d-soft"
+            }
+      },
+            "ler um livro":{
+                "titulo": "sala teletransporte",
+                "descricao": "voce encontrou sala de teletransporte, daqui voce pode escolher qualquer lugar para ir, parabéns"}
+            }
+      }, 
+            "auditorio":{
+                    "titulo": "auditorio"
+                    "descrição" : "voce esta no auditorio do insper"
+                    "opcoes":{
+                            "inicio":"voltar para o saguao de entrada",
+                              " subir palco":"subir no palco do auditorio"
+                              
+                              
+            }
+                
+       },
+                    "palco":{
+                            "titulo": "palco do audiotorio"
+                            "descricao": "voce esta no palco e apareceu um monstro"
+                            "opcoes":{
+                                    "inicio":"para descer tera que lutar contra o inimigo"
+             }
+      },
+                    "quadra":{
+                            "titulo":"quadra do insper"
+                            "descricao":"voce esta na quadra"
+                            "opcoes":{
+                                    "inicio":"tomar o elevador para ir para o saguao de entrada"
+            }
+     },
+                    "salas de estudo":{
+                            "titulo":"aquario"
+                            "descricao": "voce esta na sala de estudo em grupo"
+                            "opcoes":{
+                                    "biblioteca":"voltar para a biblioteca"
+                                    "estudar":"estudar para alguma materia qualquer"
+                            
+           }
+    },
+                            "estudar":{
+                                    "titulo":"sala de teletransporte"
+                                    "descricao":"voce encontrou sala de teletransporte, daqui voce pode escolher qualquer lugar para ir, parabéns"}
+            }
+    },
+                            "terraco":{
+                                    "titulo":"terraco insper"
+                                    "descricao":"voce esta no terraco do insper"
+                                    "opcoes":{
+                                            "cafe":"sair do terraco"
+                                            "raul":"o professor esta no terraco,lute com ele para tentar adiar o trabalho"
+             }
+   },         
+                            "cafe":{
+                                    "titulo":"cafe novo do insper"
+                                    "descricao":"voce esta no refeitorio do insper"
+                                     "opcoes":{
+                                             "quata":"sair do predio"
+                                             "terraco":"voltar para o terraco"
+            }
+   },        
+                            
+             }
+   },
+                            "raul":{
+                                    "titulo":"professor raul"
+                                    "descricao":"voce achou o professor, agora precisa lutar com ele para adiar o trabalho"
+                        
+            }
+                                          
+    },
+                           "quata":{
+                                   "titulo":"rua quata"
+                                   "descrição": " voce esta na rua do insper"
+                                   "opcoes":{
+                                           "predio 1": "ir para o predio velho",
+                                           "predio 2":"ir para o predio novo"
+            }
+    },  
+                                   "fablab":{
+                                           "titulo":"fablab"
+                                           "descricao":"voce esta em um dos laboratorios do insper"
+                                           "opcoes":{
+                                                   "quata":"sair do predio"
+                                                   "terraco": "subir para o terraco  do insper"
+                                                   "inventario":"pegar um item para melhorar o seu desempenho nas lutas"
+             }
+    }, 
+                                 "inventario":{
+                                         "titulo":"objeto para te ajudar a lutar"
+                                         "descricao":"com o objeto o seu desempenho nas batalhas sera melhor"
+             }
+                                 
+    },                            "predio 1":{
+                                          "titulo": "predio 1"
+                                          "descricao": "predio velho do insper"
+                                          "opcaoes":{
+                                                  "inicio":"ir para a entrada"
+                                                  
+            }                    
+                                    
+    },   
+                                 "predio 2":{
+                                         "titulo":"predio 2"
+                                         "descricao":"predio novo do insper"
+                                         "opcoes":{
+                                                 "fablab":"tomar o elevador para ir para o fablab"
+                                                 "terraco":"tomar o elevador para ir para o terraco"
+                                                 "sala de aula":"tomar o elevador para a sala de aula"
+            }
+     },                            
+                                "sala de aula":{
+                                        "titulo":"sala de aula 111"
+                                        "descricao":"voce esta na sala de aula"
+                                        "opcao":{
+                                                "predio 2":"tomar o elevador para descer para a entrada do predio"
+           }
+    }
+                            
+            
+    nome_cenario_atual = "inicio"
