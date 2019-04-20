9cc78832faa2725622731e69b72c30ab8cd0c42f
brunomeneghesso <brunomeneghesso@gmail.com>
2019-04-20 17:41:48 -0300

    unifiquei as fiturs de monstros e combate

--- combate_ep1.py
+++ combate_ep1.py
@@ -25,56 +25,70 @@ lista_monstros={'veterano':{"Hp":50,"atk":3,"def":15,
                         'ataques':{'nota da PI':[100,8],'o codigo das 1001 linhas':[75,15],'funcao de dano':[100,(0.5*player['Hp']+player['def'])/Raul['atk']],'inception de dicionarios':[50,20],'Prazo inadiavel':[100,20]},
                         'recompensas':{}}}
 #inicio combate (fiture de monstros)
-if monstro=='Raul':
-    print('Raul: então você quer atrasar a entrega do EP!?')
-    print('Patético, mas eu não esperava nada mais de alguém como você.')
-    print('se acha que pode me vencer lhe mostrarei o meu poder')
-else:
-    print("voce esta enfrentando um {0}".format(monstro))
-m=lista_monstros[monstro]
-Hp0=m["Hp"]
-print("ataque do inimigo {0}, seu ataque{1}".format(m["atk"],player["atk"]))
-print("defesa do inimigo {0}, sua defesa{1}".format(m["def"],player["def"]))
-print("vida do inimigo {0}, sua vida{1}".format(m["Hp"],player["Hp"]))
-while player["Hp"]>0 and m["Hp"]>0:
-    print('seus ataques são:')
-    for k,v in player["ataques"].items():
-        print("{0} precisão:{1}, dano:{2}".format(k,v[0],v[1]))
-    Atp=input("qual ataque voce quer dar? ")
-    while Atp not in player["ataques"]:
-        At=input("este nao e um ataque valido, insira um que seja ")
-    chance=randon.rendint(1,10)
-    if chance<player['ataques'][Atp][0]:
-        perda=(player['ataques'][Atp][1]*player["atk"]-m["def"])
-        if perda>0:
-            m['Hp']-=perda
-        print('voce atacou o  {0} com {1}, a vida dele esta em{2}'.format(m,At,m["Hp"]))
-    else:
-        print('voce errou o ataque')
-    Atm=random.choice(m["ataques"])
-    chance=randon.rendint(1,10)
-    if chance<m['ataques'][Atp][0]:
-        perda=(m['ataques'][Atm][1]*m["atk"]-player["def"])
-        if perda>0:
-            player['Hp']-=perda
-        print('{0} atacou voce com {1}, a sua vida esta em{2}'.format(m,At,player["Hp"]))
-    else:
-        print('o inimigo tentou atacar voce com {0}, mas errou'.format(Atm))
-if player['Hp']<=0:
-    print('voce foi derrotado pelo {0}'.format(m))
-    game_over=True
-else:
+rolagem=random. rendint(0,100)
+if rolagem<=cenario_atual['probabilidade']:
+    monstro=random.choice(cenario_atual['monstros possiveis'])
+    combate=True
+   
+if combate:
     if monstro=='Raul':
-        print('Raul: Impossivel! Como um ser inferior conseguiu me ferir')
-        print('Parabés seu verme insolente, você foi capaz de superar 1% do meu poder.')
-        print('como prova de seu feito adiarei a entrega do EP1')
-        print('mas não terei tanta misericórdia no EP final')
-        print('esteja pronto')
+        print('Raul: então você quer atrasar a entrega do EP!?')
+        time.sleep(0.5)
+        print('Patético, mas eu não esperava nada mais de alguém como você.')
+        time.sleep(0.5)
+        print('se acha que pode me vencer lhe mostrarei o meu poder')
+    else:
+        print("voce esta enfrentando um {0}".format(monstro))
+    m=lista_monstros[monstro]
+    Hp0=m["Hp"]
+    print("ataque do inimigo {0}, seu ataque{1}".format(m["atk"],player["atk"]))
+    print("defesa do inimigo {0}, sua defesa{1}".format(m["def"],player["def"]))
+    print("vida do inimigo {0}, sua vida{1}".format(m["Hp"],player["Hp"]))
+    while player["Hp"]>0 and m["Hp"]>0:
+        print('seus ataques são:')
+        for k,v in player["ataques"].items():
+            print("{0} precisão:{1}, dano:{2}".format(k,v[0],v[1]))
+        Atp=input("qual ataque voce quer dar? ")
+        while Atp not in player["ataques"]:
+            At=input("este nao e um ataque valido, insira um que seja ")
+        chance=randon.rendint(1,10)
+        if chance<player['ataques'][Atp][0]:
+            perda=(player['ataques'][Atp][1]*player["atk"]-m["def"])
+            if perda>0:
+                m['Hp']-=perda
+                print('voce atacou o  {0} com {1}, a vida dele esta em{2}'.format(m,At,m["Hp"]))
+            else:
+                print('voce errou o ataque')
+        Atm=random.choice(m["ataques"])
+        chance=randon.rendint(1,10)
+        if chance<m['ataques'][Atp][0]:
+            perda=(m['ataques'][Atm][1]*m["atk"]-player["def"])
+            if perda>0:
+                player['Hp']-=perda
+            print('{0} atacou voce com {1}, a sua vida esta em{2}'.format(m,At,player["Hp"]))
+        else:
+            print('o inimigo tentou atacar voce com {0}, mas errou'.format(Atm))
+    if player['Hp']<=0:
+        print('voce foi derrotado pelo {0}'.format(m))
         game_over=True
-        win=True
-    print('voce derrotou o {0}, aqui estão seus premios'.format(m))
-    for k,v in m['recompensas'].items:
-        print('voce recebu {0} de {1}'.format(v,k))
-        player[k]+=v
-    m['Hp']=Hp0
+    else:
+        if monstro=='Raul':
+            print('Raul: Impossivel! Como um ser inferior conseguiu me ferir')
+            time.sleep(0.5)
+            print('Parabés seu verme insolente, você foi capaz de superar 1% do meu poder.')
+            time.sleep(0.5)
+            print('como prova de seu feito adiarei a entrega do EP1')
+            time.sleep(0.5)
+            print('mas não terei tanta misericórdia no EP final')
+            time.sleep(0.5)
+            print('esteja pronto')
+            game_over=True
+            win=True
+        else:
+            print('voce derrotou o {0}, aqui estão seus premios'.format(m))
+            for k,v in m['recompensas'].items:
+                print('voce recebu {0} de {1}'.format(v,k))
+                player[k]+=v
+            m['Hp']=Hp0
+        combate=False
     #linha adicional necessária para fiture de item
