709f109c6de89f182be1755cff86e34c9a517591
brunomeneghesso <brunomeneghesso@gmail.com>
2019-04-21 23:28:38 -0300

    corrigi os erros

--- inventario.py
+++ inventario.py
@@ -1,12 +1,12 @@
-itens={'espada':{'descricao':'aumenta o dano de ataque','atk':5},'chave auditorio':{'descricao':'permite acesso ao auditorio','modificacoes':[]},'forca':{'descricao':'aumenta o poder de ataque','modificacoes':'atk':6},'armadura':{'descricao':'aumenta a protecao','modificacoes':{'def':5}},'bonus':{'descricao':'funcoes adicionais ','modificacoes':'atk def Hp ou cura': 4}}
+itens={'espada':{'descricao':'aumenta o dano de ataque','modificacoes':{'atk':5}},'chave auditorio':{'descricao':'permite acesso ao auditorio','modificacoes':[]},'forca':{'descricao':'aumenta o poder de ataque','modificacoes':{'atk':6}},'armadura':{'descricao':'aumenta a protecao','modificacoes':{'def':5}},'bebida revigorante':{'descricao':'funcoes adicionais ','modificacoes':{'cura': 4}}}
 inventario={}
 
 
-    if cenario_atual['item'] not in itens:
-       inventario[cenario_[itens]]=itens[cenario_atual['itens']]
-        for k,v in inventario:
-            print("agora voce tem{0}{1}".format(cenario_atual['itens'],itens[cenario_atuar['itens']['descricao']]))
-            print ("itens")
+if cenario_atual['item'] not in itens:
+    inventario[cenario_atual[itens]]=itens[cenario_atual['itens']]
+    for k,v in inventario:
+        print("agora voce tem{0}{1}".format(cenario_atual['itens'],itens[cenario_atual['itens']['descricao']]))
+        print (inventario)
 
 
 
