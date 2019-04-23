6b5c68621586a83023a91ebde5d06b24d40e2dc6
unknown <laurabatmanp@gmail.com>
2019-04-22 22:41:23 -0300

    modifiquei ep1

--- ep1.py
+++ ep1.py
@@ -192,12 +192,6 @@ def carregar_cenarios():
     
                               itens={'espada':{'descricao':'aumenta o dano de ataque','atk':5},'chave auditorio':{'descricao':'permite acesso ao auditorio','modificacoes':[]},'forca':{'descricao':'aumenta o poder de ataque','modificacoes':'atk':6},'armadura':{'descricao':'aumenta a protecao','modificacoes':{'def':5}},'bonus':{'descricao':'funcoes adicionais ','modificacoes':'atk def Hp ou cura': 4}}
                               
-                              
-    if cenario_atual['item'] not in itens:
-       inventario[cenario_[itens]]=itens[cenario_atual['itens']]
-        for k,v in inventario:
-            print("agora voce tem{0}{1}".format(cenario_atual['itens'],itens[cenario_atuar['itens']['descricao']]))
-            print ("itens")
 
 
 
@@ -266,6 +260,16 @@ def main():
         print(cenario_atual["titulo"])
         print("-"*len(cenario_atual["titulo"]))
         print(cenario_atual["descricao"])
+        
+        
+    if cenario_atual['item'] not in itens:
+       inventario[cenario_[itens]]=itens[cenario_atual['itens']]
+        for k,v in inventario:
+            print("agora voce tem{0}{1}".format(cenario_atual['itens'],itens[cenario_atuar['itens']['descricao']]))
+            print ("itens")
+
+
+
         
         
         if nome_cenario_atual=="ler um livro":
